c0beca7d2158bc43e82df991f7c412272fcb321f
Vo Thu Thao <0945206559vtt@gmail.com>
2026-06-07 17:41:49 +0700

    Update: Logic sync and UIShop

--- Assets/_Game/Scripts/Core/Events/Boss/BossEventBus.cs
+++
@@ -1,9 +0,0 @@
-// using System;
-// using UnityEngine;
-// // public static class BossEventBus
-// // {
-// //     public static Action<GameObject> OnBossSpawned; // ← thêm dòng này
-// //     public static Action<ElementType> OnElementChanged;
-// //     public static Action<float> OnPhaseChanged;
-// //     public static Action OnBossDeath;
-// // }
